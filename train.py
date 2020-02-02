cdb742e25c813d6ca47d5bc166050491d1c2e38a
Jungdae Kim <kekmodel@gmail.com>
2020-02-02 23:46:55 +0900

    Update DDP

--- train.py
+++ train.py
@@ -65,7 +65,7 @@ def main():
     parser = argparse.ArgumentParser(description='PyTorch FixMatch Training')
     parser.add_argument('--gpu-id', default='0', type=int,
                         help='id(s) for CUDA_VISIBLE_DEVICES')
-    parser.add_argument('--num-workers', type=int, default=0,
+    parser.add_argument('--num-workers', type=int, default=4,
                         help='Number of workers')
     parser.add_argument('--dataset', default='cifar10', type=str,
                         choices=['cifar10', 'cifar100'],
