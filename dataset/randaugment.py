32082a7182694353674aa7ec7b99d40739d8f251
Jungdae Kim <kekmodel@gmail.com>
2020-02-05 19:22:13 +0900

    Test augment

--- dataset/randaugment.py
+++ dataset/randaugment.py
@@ -47,7 +47,7 @@ def Cutout(img, v, max_v, bias=0):
 def CutoutAbs(img, v, **kwarg):
     w, h = img.size
     x0 = np.random.uniform(0, w)
-    y0 = np.random.unifor(0, h)
+    y0 = np.random.uniform(0, h)
     x0 = int(max(0, x0 - v / 2.))
     y0 = int(max(0, y0 - v / 2.))
     x1 = int(min(w, x0 + v))
